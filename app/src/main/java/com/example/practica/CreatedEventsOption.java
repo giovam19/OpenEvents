527ff8a08b9bcb71ba2a7ccc4ea93f36492fa12d
Inés Graells <ines.graells@students.salle.url.edu>
2021-05-10 11:35:03 +0200

    clases menú 4

--- app/src/main/java/com/example/practica/CreatedEventsOption.java
+++ app/src/main/java/com/example/practica/CreatedEventsOption.java
@@ -1,6 +1,24 @@
 package com.example.practica;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreatedEventsOption extends AppCompatActivity {
+    private ImageView backButton;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_personal_events);
+
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.light_blue));
+    }
 }
--- app/src/main/java/com/example/practica/SavedEventsOption.java
+++ app/src/main/java/com/example/practica/SavedEventsOption.java
@@ -1,6 +1,26 @@
 package com.example.practica;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SavedEventsOption extends AppCompatActivity {
+    private ImageView backButton;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_personal_events);
+
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.light_blue));
+
+
+    }
 }
